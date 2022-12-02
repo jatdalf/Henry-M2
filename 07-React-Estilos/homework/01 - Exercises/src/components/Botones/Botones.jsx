cb51a5e7b149701c366f56290b7967c378b3ef33
Jorge Toso <jorgetoso@hotmail.com>
2022-12-02 20:07:54 -0300

    React Styles

--- 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
+++ 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
-
-const DivButtons = 'DivButtons'
-const Buttons = 'Buttons'
+const DivButtons = styled.div `
+display:flex ;
+flex-direction:row ;
+justify-content:space-around ;
+align-items:center ;
+`
+const Buttons = styled.button`
+font-weight:bold ;
+background-color:#8ef6e4 ;
+border-radius:5px ;
+margin:10px ;
+padding:5px ;
+&:hover{
+  background-color: #262623c9;
+  color: #8ef6e4;
+  cursor: pointer;
+  }
+`
 
 export default class Botones extends React.Component {
   render () {
     const { alerts } = this.props
     return (
-      <div>
-        <button onClick={() => window.alert(alerts.m1)}>Módulo 1</button>
-        <button onClick={() => window.alert(alerts.m2)}>Módulo 2</button>
-      </div>
+      <DivButtons>
+        <Buttons onClick={() => window.alert(alerts.m1)}>Módulo 1</Buttons>
+        <Buttons onClick={() => window.alert(alerts.m2)}>Módulo 2</Buttons>
+      </DivButtons>
     )
   }
 }
