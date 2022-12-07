46eea5630665ebdf7111f17d77527582304385b4
Jorge Toso <jorgetoso@hotmail.com>
2022-12-06 21:11:59 -0300

    React-Routing

--- 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
+++ 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import logo from "../../assets/logo.png";
 import logoHenry from "../../assets/logo-henry.png";
 import styleNav from "./NavBar.module.css";
+import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className={styleNav.container}>
       <ul className={styleNav.menu}>
+        <NavLink to='/'>
         <li>
           <img src={logoHenry} alt="logo-henry" />
           <img src={logo} alt="logo" />
         </li>
+        </NavLink>
+        
         <li>
           <h1>Central de Cruceros</h1>
-        </li>
+        </li>        
         <div className={styleNav.options}>
           <li>
+          <NavLink to='/shipping'>
             <span>Navieras</span>
+          </NavLink>
+            
           </li>
           <li>
-            <span>Promociones</span>
+            <NavLink to='/promotions'>
+              <span>Promociones</span>
+            </NavLink>
+            
           </li>
         </div>
       </ul>
